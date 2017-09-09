d72ad246b8434532273eecb9dbee20f1d9c7f169
bobo_yiguo_shi <shixiaobo8@163.com>
2017-09-09 18:36:44 +0800

    我的视频页面下载和搜索初步实现

--- video_omsa/video_omsa/settings.py
+++ video_omsa/video_omsa/settings.py
@@ -16,7 +16,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SERVER_DOMAIN = "http://www.devops89.cn:81"
+SERVER_DOMAIN = "localhost"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -216,4 +216,5 @@ NEVER_REDIS_TIMEOUT=365*24*60*60
 
 #### 全局配置文件 #####
 ## mp4 服务器视频上传资源位置
-MP4_SERVER_DIR = 'D:/bjyjy' + u"/要上传的视频"
+MP4_SERVER_DIR = "H:/" + u"盈佳科技视频资源" + "/J005-SSH_MP4"
+MP4_UPLOAD_DIR = "E:/www/yjy_django_video/UPLOADS"
