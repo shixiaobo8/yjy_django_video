e4a6a1cbb78f5e27a47b1f498e4a7fdfa6bc6abe
shixiaobo8 <shixiaobo8@163.com>
2017-09-20 15:57:46 +0800

    celery 配置

--- video_omsa/video/tasks.py
+++ video_omsa/video/tasks.py
@@ -3,7 +3,7 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-
+import time
 
 @shared_task
 def add(x, y):
@@ -18,3 +18,8 @@ def mul(x, y):
 @shared_task
 def xsum(numbers):
     return sum(numbers)
+
+@shared_task
+def sleep(s):
+    time.sleep(s)
+    return 'ok'
