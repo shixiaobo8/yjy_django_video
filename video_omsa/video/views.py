202671e26274627d60f9199fe87551b1c00b2e2a
shixiaobo8 <shixiaobo8@163.com>
2017-10-09 11:22:12 +0800

    taskbug

--- video_omsa/video/views.py
+++ video_omsa/video/views.py
@@ -1385,16 +1385,17 @@ def task_detail(request):
 # 检查MP4状态
 @csrf_exempt
 def checkMp4Status():
-    sql = "select `id`,`cut_id` from yjy_mp4 where `cut_staus` not in ('0','1','2','3','4','5','6');"
+    sql = "select `id`,`cut_id` from yjy_mp4 where `cut_staus`='2';"
     print sql
     rs = executeSql(sql)
     for r in rs:
         sql1 = "select `result` from `django_celery_results_taskresult` where `task_id`='%s'"%(r[1])
         result = executeSql(sql1)[0]
         print sql1,result
-        sql2 = "update `yjy_mp4` set `cut_status`='7' where id='%s'"%(r[0])
-        print sql2
-        executeSql(sql2)
+        if result != '切片顺利完成!':
+            sql2 = "update `yjy_mp4` set `cut_status`='7' where id='%s'"%(r[0])
+            print sql2
+            executeSql(sql2)
 
 
 # 批量任务切片
