650ddff264f2d06115caec2288a0137addea6970
shixiaobo8 <shixiaobo8@163.com>
2017-10-09 17:51:50 +0800

    taskbug

--- video_omsa/video/views.py
+++ video_omsa/video/views.py
@@ -1437,7 +1437,7 @@ def mp4AferCut(request):
         video_id = request.GET.get('id',None)
         sql = "select `id`,`video_id`,`thumb_url`,`resolution`,`duration`,`m3u8_serverPath`,`aes_m3u8_serverPath`,`file_size`,`cut_time`,`status` from `mp4_cut_recoder`"
         rs = executeSql(sql)
-        appinfos = str(getApptypes(video_id)[0]).replace('(','').replace(')','').split(',')
+        appinfos = str(getApptypes(video_id)[0]).replace('(','').replace(')','').replace("u'",'').replace('L','').split(',')
         apptype_v = getApptypeName(appinfos[0])
         parent_id = getAppTitle(appinfos[0],video_id)
         chapter_id = getAppTitle(appinfos[0],video_id)
