5d16a0588cc06a48f99221b5831b150fcece6a67
shixiaobo8 <shixiaobo8@163.com>
2017-10-11 12:01:06 +0800

    taskbug

--- video_omsa/video/views.py
+++ video_omsa/video/views.py
@@ -1442,7 +1442,7 @@ def mp4AferCut(request):
         apptype_v = getApptypeName(appinfos[0])
         parent = {'id':int(appinfos[1]),'name':getAppTitle(appinfos[0],appinfos[1])}
         chapter = {'id':int(appinfos[2]),'name':getAppTitle(appinfos[0],appinfos[2])}
-        section = {'id':int(appinfos[3]),'name':getAppSectionOneTitle(appinfos[0],appinfos[3])}
+        section = {'id':appinfos[3],'name':getAppSectionOneTitle(appinfos[0],appinfos[3])}
         chinese_name = appinfos[4]
         print chinese_name
         tmp = []
