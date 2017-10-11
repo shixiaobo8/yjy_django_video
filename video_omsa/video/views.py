13568be5500f6bfea93b1c9fe927a9e867c23f53
shixiaobo8 <shixiaobo8@163.com>
2017-10-11 12:03:05 +0800

    taskbug

--- video_omsa/video/views.py
+++ video_omsa/video/views.py
@@ -1444,7 +1444,7 @@ def mp4AferCut(request):
         chapter = {'id':int(appinfos[2]),'name':getAppTitle(appinfos[0],appinfos[2])}
         section = {'id':appinfos[3],'name':getAppSectionOneTitle(appinfos[0],appinfos[3])}
         chinese_name = appinfos[4]
-        print chinese_name
+        print appinfos
         tmp = []
         for r in rs:
             tmp1 = dict()
