eaf17713fcb97476b57ed3ae2dae5783324f5c76
bobo_yiguo_shi <shixiaobo8@163.com>
2017-10-24 08:33:16 +0800

    taskbug

--- video_omsa/video/tasks.py
+++ video_omsa/video/tasks.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 import time,commands
 from .ffmpeg import Aes,ffmpeg
-from .views import sendMail
+# from .views import sendMail
 
 @shared_task
 def add(x, y):
@@ -33,7 +33,7 @@ def cut_video(task_id, video_id,mp4_path):
      return res
 
 # 发送邮件
-@shared_task
-def send_Mail(task_id,video_id,status):
-    res = sendMail(task_id,video_id,status)
-    return res
+# @shared_task
+# def send_Mail(task_id,video_id,status):
+#     res = sendMail(task_id,video_id,status)
+#     return res
