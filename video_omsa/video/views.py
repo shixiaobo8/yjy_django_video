d154dcb80c16a22b4c8a5dd2c9dfad3ccb0663e5
bobo_yiguo_shi <shixiaobo8@163.com>
2017-10-28 11:19:25 +0800

    shareGame

--- video_omsa/video/views.py
+++ video_omsa/video/views.py
@@ -226,7 +226,7 @@ def shareGame(request):
           'ios_download': 'apk/YinJiaPengHu.apk',
           'android_download': 'apk/YinJiaPengHu.apk',
           'timeout': 1000};
-         return HttpResponse("<a href='yinjiapenghu://com.yinjia.penghu?mid=0&roomid=" + roomid + "&peoples=" + peoples + "'>" +"分享链接" + "</a>")
+         return HttpResponse("<a href='yinjiapenghu://com.yinjia.penghu?mid=0&roomid=" + roomid + "&peoples=" + peoples + "'>" +"shareGameUrl" + "</a>")
 
 
 def test_inter(request):
